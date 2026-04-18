b7c4588485af6a1b8d4539e0688aa3a1a203de7e
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-04-18 15:47:49 +0800

    feat: try useMemo

--- react-advance/src/App.tsx
+++ react-advance/src/App.tsx
@@ -1,17 +1,29 @@
+import { useMemo, useState } from "react"
 import BlogCard from "./components/BlogCard"
 
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
   const blog = {
     title: "Blog dummy",
     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, quis!"
   }
+  const result = useMemo(() => { // digunakan untuk menyimpan hasil perhitungan agar tidak dihitung ulang setiap render.
+    return count *2;
+  }, [count])
+
 
   return (
     <>
 
     <BlogCard blog={blog} />
+
+    <div>
+      <button className="bg-sky-500 p-2 text-white" onClick={()=> setCount(prev => prev + 1)}>update</button>
+      <h1>{result}</h1>
+    </div>
       
     </>
   )
