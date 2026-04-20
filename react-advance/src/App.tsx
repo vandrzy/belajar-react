11c75dec676be36801f3fa234fe31bdd9073b67f
Vandy Rizky Septiawan <vandyrizky33@gmail.com>
2026-04-20 21:32:36 +0800

    lazy & suspense

--- react-advance/src/App.tsx
+++ react-advance/src/App.tsx
@@ -1,18 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Contact from "./pages/Contact"
+import {Route, Routes } from "react-router-dom"
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => (import("./pages/Home")))
+const About = lazy(() => (import("./pages/About")))
+const Contact = lazy(() => (import("./pages/Contact")))
 
 
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
+      <Suspense fallback={<div>loading ....</div>}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </Suspense>
+
   )
 }
 
